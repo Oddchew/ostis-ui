11ddb50d47d1d68cb3afc94971105463c3d6c53f
MksmOrlov <orlovmaksimkonstit@gmail.com>
2024-04-08 14:17:40 +0300

    test: add SpecifiedStringTemplateAgentTest

--- template-agent/tests/units/SpecifiedStringTemplateAgentTest.cpp
+++ template-agent/tests/units/SpecifiedStringTemplateAgentTest.cpp
@@ -4,96 +4,116 @@
  * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
  */
 
-#include "renderer/StringTemplateRenderer.hpp"
-
 #include "sc_test.hpp"
 #include "scs_loader.hpp"
 
-#include "keynodes/SpecifiedStringTemplateKeynodes.hpp"
 #include "sc-agents-common/keynodes/coreKeynodes.hpp"
+#include "sc-agents-common/utils/AgentUtils.hpp"
+#include "sc-agents-common/utils/IteratorUtils.hpp"
+#include "sc-memory/kpm/sc_agent.hpp"
+
+#include "keynodes/SpecifiedStringTemplateKeynodes.hpp"
+#include "agent/SpecifiedStringTemplateAgent.hpp"
 
 using namespace specifiedStringTemplateModule;
+using namespace scAgentsCommon;
 
 namespace templateAgentTest
 {
 std::string const TEST_FILES_DIR_PATH = TEMPLATE_AGENT_TEST_SRC_PATH "/test-structures/";
-
+int const WAIT_TIME = 1000;
 using TemplateAgentTest = ScMemoryTest;
+
+void InitializeTest()
+{
+  CoreKeynodes::InitGlobal();
+  SpecifiedStringTemplateKeynodes::InitGlobal();
+
+  ScAgentInit(true);
+  SC_AGENT_REGISTER(SpecifiedStringTemplateAgent);
+}
+
+void ShutdownTest()
+{
+  SC_AGENT_UNREGISTER(SpecifiedStringTemplateAgent);
+}
 
 TEST_F(TemplateAgentTest, ZeroVariavlesStringTemplate)
 {
+  InitializeTest();
+
   ScMemoryContext & context = *m_ctx;
-  scAgentsCommon::CoreKeynodes::InitGlobal();
-  SpecifiedStringTemplateKeynodes::InitGlobal();
   ScsLoader loader;
   loader.loadScsFile(context, TEST_FILES_DIR_PATH + "zero_variables_string_template.scs");
 
-  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
-  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
-  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
+  // Call the agent, get and validate result
+  ScAddr test_action_node = context.HelperFindBySystemIdtf("test_action_node");
+  EXPECT_TRUE(context.IsElement(test_action_node));
+  ScAddr result = utils::AgentUtils::applyActionAndGetResultIfExists(&context, test_action_node, WAIT_TIME);
+  EXPECT_TRUE(context.IsElement(result));
+  ScAddr resultLink = utils::IteratorUtils::getAnyFromSet(&context, result);
+  EXPECT_TRUE(context.IsElement(resultLink));
 
-  std::string expectedResult;
+  std::string resultLinkContent;
+  context.GetLinkContent(resultLink, resultLinkContent);
+  EXPECT_NE(resultLinkContent, "");
+
+  // Check if the result is correct
   ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
-  context.GetLinkContent(string_template_expected_result, expectedResult);
+  std::string string_template_expected_result_content;
+  context.GetLinkContent(string_template_expected_result, string_template_expected_result_content);
+  EXPECT_EQ(string_template_expected_result_content, resultLinkContent);
 
-  EXPECT_EQ(result, expectedResult);
+  // Check if nrel_format is generated for the result sc-link
+  ScIterator5Ptr formatIterator = context.Iterator5(
+        resultLink,
+        ScType::EdgeDCommonConst,
+        context.HelperFindBySystemIdtf("format_html"),
+        ScType::EdgeAccessConstPosPerm,
+        SpecifiedStringTemplateKeynodes::nrel_format);
+  EXPECT_TRUE(formatIterator->Next());
+
+  ShutdownTest();
 }
+
 
 TEST_F(TemplateAgentTest, OneVariavleStringTemplate)
 {
+  InitializeTest();
+
   ScMemoryContext & context = *m_ctx;
-  scAgentsCommon::CoreKeynodes::InitGlobal();
-  SpecifiedStringTemplateKeynodes::InitGlobal();
   ScsLoader loader;
   loader.loadScsFile(context, TEST_FILES_DIR_PATH + "one_variable_string_template.scs");
 
-  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
-  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
-  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
+  // Call the agent, get and validate result
+  ScAddr test_action_node = context.HelperFindBySystemIdtf("test_action_node");
+  EXPECT_TRUE(context.IsElement(test_action_node));
+  ScAddr result = utils::AgentUtils::applyActionAndGetResultIfExists(&context, test_action_node, WAIT_TIME);
+  EXPECT_TRUE(context.IsElement(result));
+  ScAddr resultLink = utils::IteratorUtils::getAnyFromSet(&context, result);
+  EXPECT_TRUE(context.IsElement(resultLink));
 
-  std::string expectedResult;
+  // Check if the result is correct
+  std::string resultLinkContent;
+  context.GetLinkContent(resultLink, resultLinkContent);
+  EXPECT_NE(resultLinkContent, "");
+
   ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
-  context.GetLinkContent(string_template_expected_result, expectedResult);
+  std::string string_template_expected_result_content;
+  context.GetLinkContent(string_template_expected_result, string_template_expected_result_content);
 
-  EXPECT_EQ(result, expectedResult);
+  // Check if nrel_format is generated for the result sc-link
+  EXPECT_EQ(string_template_expected_result_content, resultLinkContent);
+  ScIterator5Ptr formatIterator = context.Iterator5(
+        resultLink,
+        ScType::EdgeDCommonConst,
+        context.HelperFindBySystemIdtf("format_html"),
+        ScType::EdgeAccessConstPosPerm,
+        SpecifiedStringTemplateKeynodes::nrel_format);
+  EXPECT_TRUE(formatIterator->Next());
+
+  ShutdownTest();
 }
 
-TEST_F(TemplateAgentTest, TwoVariavlesStringTemplate)
-{
-  ScMemoryContext & context = *m_ctx;
-  scAgentsCommon::CoreKeynodes::InitGlobal();
-  SpecifiedStringTemplateKeynodes::InitGlobal();
-  ScsLoader loader;
-  loader.loadScsFile(context, TEST_FILES_DIR_PATH + "two_variables_string_template.scs");
-
-  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
-  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
-  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
-
-  std::string expectedResult;
-  ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
-  context.GetLinkContent(string_template_expected_result, expectedResult);
-
-  EXPECT_EQ(result, expectedResult);
-}
-
-TEST_F(TemplateAgentTest, ThreeVariavlesStringTemplate)
-{
-  ScMemoryContext & context = *m_ctx;
-  scAgentsCommon::CoreKeynodes::InitGlobal();
-  SpecifiedStringTemplateKeynodes::InitGlobal();
-  ScsLoader loader;
-  loader.loadScsFile(context, TEST_FILES_DIR_PATH + "three_variables_string_template.scs");
-
-  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
-  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
-  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
-
-  std::string expectedResult;
-  ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
-  context.GetLinkContent(string_template_expected_result, expectedResult);
-
-  EXPECT_EQ(result, expectedResult);
-}
 
 } // namespace templateAgentTest
---
+++ template-agent/tests/units/StringTemplateRendererTest.cpp
@@ -0,0 +1,100 @@
+/*
+ * This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+ * Distributed under the MIT License
+ * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include "sc_test.hpp"
+#include "scs_loader.hpp"
+
+#include "sc-agents-common/keynodes/coreKeynodes.hpp"
+
+#include "renderer/StringTemplateRenderer.hpp"
+#include "keynodes/SpecifiedStringTemplateKeynodes.hpp"
+
+
+using namespace specifiedStringTemplateModule;
+
+namespace RendererTest
+{
+std::string const TEST_FILES_DIR_PATH = TEMPLATE_AGENT_TEST_SRC_PATH "/test-structures/";
+
+using RendererTest = ScMemoryTest;
+
+TEST_F(RendererTest, ZeroVariavlesStringTemplate)
+{
+  ScMemoryContext & context = *m_ctx;
+  scAgentsCommon::CoreKeynodes::InitGlobal();
+  SpecifiedStringTemplateKeynodes::InitGlobal();
+  ScsLoader loader;
+  loader.loadScsFile(context, TEST_FILES_DIR_PATH + "zero_variables_string_template.scs");
+
+  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
+  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
+  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
+
+  std::string expectedResult;
+  ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
+  context.GetLinkContent(string_template_expected_result, expectedResult);
+
+  EXPECT_EQ(result, expectedResult);
+}
+
+TEST_F(RendererTest, OneVariavleStringTemplate)
+{
+  ScMemoryContext & context = *m_ctx;
+  scAgentsCommon::CoreKeynodes::InitGlobal();
+  SpecifiedStringTemplateKeynodes::InitGlobal();
+  ScsLoader loader;
+  loader.loadScsFile(context, TEST_FILES_DIR_PATH + "one_variable_string_template.scs");
+
+  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
+  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
+  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
+
+  std::string expectedResult;
+  ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
+  context.GetLinkContent(string_template_expected_result, expectedResult);
+
+  EXPECT_EQ(result, expectedResult);
+}
+
+TEST_F(RendererTest, TwoVariavlesStringTemplate)
+{
+  ScMemoryContext & context = *m_ctx;
+  scAgentsCommon::CoreKeynodes::InitGlobal();
+  SpecifiedStringTemplateKeynodes::InitGlobal();
+  ScsLoader loader;
+  loader.loadScsFile(context, TEST_FILES_DIR_PATH + "two_variables_string_template.scs");
+
+  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
+  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
+  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
+
+  std::string expectedResult;
+  ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
+  context.GetLinkContent(string_template_expected_result, expectedResult);
+
+  EXPECT_EQ(result, expectedResult);
+}
+
+TEST_F(RendererTest, ThreeVariavlesStringTemplate)
+{
+  ScMemoryContext & context = *m_ctx;
+  scAgentsCommon::CoreKeynodes::InitGlobal();
+  SpecifiedStringTemplateKeynodes::InitGlobal();
+  ScsLoader loader;
+  loader.loadScsFile(context, TEST_FILES_DIR_PATH + "three_variables_string_template.scs");
+
+  ScAddr string_template = context.HelperFindBySystemIdtf("string_template");
+  ScAddr replacement_values = context.HelperFindBySystemIdtf("replacement_values");
+  std::string result = specifiedStringTemplateModule::StringTemplateRenderer::RenderStringTemplate(context, string_template, replacement_values);
+
+  std::string expectedResult;
+  ScAddr string_template_expected_result = context.HelperFindBySystemIdtf("string_template_expected_result");
+  context.GetLinkContent(string_template_expected_result, expectedResult);
+
+  EXPECT_EQ(result, expectedResult);
+}
+
+} // namespace RendererTest
